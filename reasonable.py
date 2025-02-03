db88053f3c288cd3aeebf4633907b7a3ad072157
colon3hasasymbol <fsapora@outlook.com>
2025-02-02 19:51:12 -0500

    gitingntjrdor

--- reasonable.py
+++
@@ -1,6 +0,0 @@
-import os, sys, time, websocket, requests
-
-control_request = requests.get(url="http://lunch.gay/control")
-control_data = control_request.raw()
-
-print(control_data)
--- src/multiplayer.zig
+++
@@ -1,3 +0,0 @@
-const std = @import("std");
-
-pub fn multiplayer() !void {}
